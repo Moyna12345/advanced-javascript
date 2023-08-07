b637bbc69b931cb39e523c330184a9bb8e101cc2
Moyna <faima.moyna@itchum.xyz>
2023-08-07 16:57:34 +0600

    true vs false

--- array-split.js
+++ array-split.js
@@ -1,4 +1,6 @@
 const nums = [1,2,3,4,5,6,7,8];
-const part = nums.slice(2, 5);
-console.log(part);
+// const part = nums.slice(2, 5);
+
+const removed = nums.splice(2, 3, 77, 88, 99);
+console.log(removed);
 console.log(nums);
